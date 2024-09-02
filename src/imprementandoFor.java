a84ecaa33c4ceb3703a2c79ae6d4380472851c28
Otoniel <tonyernane@gmail.com>
2024-09-02 12:09:31 -0300

    first commit

---
+++ src/imprementandoFor.java
@@ -0,0 +1,8 @@
+public class imprementandoFor {
+public static void main(String[] args) {
+    for (int boizinhos = 1; boizinhos <= 20; boizinhos++){
+        System.out.println("contando boizinhos " + boizinhos);
+    }
+        System.out.println("zelinha dormiu");
+}
+}
